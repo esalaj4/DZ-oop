b44c0ba80bea74e97ce15479b1a92fcdab917d95
esalaj4 <86615856+esalaj4@users.noreply.github.com>
2021-11-30 16:23:34 +0100

    Add files via upload

---
+++ DZ2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Logika;
+using System.Collections.Generic;
+using System.IO;
+using  System.Globalization;
+
+namespace OOP_DZ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime monday = new DateTime(2021, 11, 8);
+            Weather mondayWeather = new Weather(6.17, 56.13, 4.9);
+            DailyForecast mondayForecast = new DailyForecast(monday, mondayWeather);
+            Console.WriteLine(monday.ToString());
+            Console.WriteLine(mondayWeather.GetAsString());
+            Console.WriteLine(mondayForecast.GetAsString());
+
+
+            string fileName = "weather.forecast";
+            
+
+            if (File.Exists(fileName) == false)
+            {
+                Console.WriteLine("The required file does not exist. Please create it, or change the path.");
+                return;
+            }
+
+            string[] dailyWeatherInputs = File.ReadAllLines(fileName);
+
+            DailyForecast[] dailyForecasts = new DailyForecast[dailyWeatherInputs.Length];
+            for (int i = 0; i < dailyForecasts.Length; i++)
+            {
+                dailyForecasts[i] = ForecastUtilities.Parse(dailyWeatherInputs[i]);
+
+            }
+
+            WeeklyForecast weeklyForecast = new WeeklyForecast(dailyForecasts);
+            Console.WriteLine(weeklyForecast.GetAsString());
+
+            Console.WriteLine("Maximal weekly temperature:");
+            Console.WriteLine(weeklyForecast.GetMaxTemperature());
+            Console.WriteLine(weeklyForecast[0].GetAsString());
+        }
+    }
+}
---
+++ DZ2/WeeklyForecast.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Logika
+{
+    public class WeeklyForecast
+    {
+        private DailyForecast[] dailyForecasts = new DailyForecast[7];
+
+        public WeeklyForecast(DailyForecast[] daily)
+        {
+            this.dailyForecasts = daily;
+
+        }
+
+        public string GetAsString()
+        {
+
+            string[] s = new string[7];
+            s[0] = dailyForecasts[0].GetAsString();
+            s[1] = dailyForecasts[1].GetAsString();
+            s[2] = dailyForecasts[2].GetAsString();
+            s[3] = dailyForecasts[3].GetAsString();
+            s[4] = dailyForecasts[4].GetAsString();
+            s[5] = dailyForecasts[5].GetAsString();
+            s[6] = dailyForecasts[6].GetAsString();
+
+            return string.Join("\n", s);
+        }
+
+        public double GetMaxTemperature()
+        {
+            DailyForecast maxT = dailyForecasts[0];
+            DailyForecast temp;
+            double max = 0;
+
+            for (int i = 0; i < dailyForecasts.Length; i++)
+            {
+                temp = dailyForecasts[i];
+
+                if (temp > maxT == true)
+                {
+                    maxT = temp;
+                    max = maxT.GetTemperature();
+                }
+
+            }
+            return max;
+
+        }
+
+        public DailyForecast this[int index]
+        {
+            get { return dailyForecasts[index]; }
+        }
+    }
+}
